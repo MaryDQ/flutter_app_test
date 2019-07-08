c0a3540bf6d394f1db46f66e712bce2a3dad9f9a
party <80731595@qq.com>
2019-07-08 15:33:02 +0800

    1.第一个Flutter应用程序，学习到了第三步

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-//import 'package:english_words/english_words.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -17,9 +18,24 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+//          child: new Text('Hello World'),
+//          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
